24f2cfd522e810d8bd732c8f1ebcf8ceae4c79eb
cyonits <cyonitss@gmail.com>
2023-07-27 14:08:05 +0900

    first commit

---
+++ src/main/java/codestates/CIHello/CiHelloApplication.java
@@ -0,0 +1,13 @@
+package codestates.CIHello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiHelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiHelloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/codestates/CIHello/CiHelloApplicationTests.java
@@ -0,0 +1,13 @@
+package codestates.CIHello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiHelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
